4238c98b909e20dbfe5edba3a5143eb50327dcf0
MorosDaniel <dani28022016@gmail.com>
2025-01-14 16:32:45 -0500

    Envoriment

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -11,6 +11,13 @@ def call(Map config){
             maven  "maven3"
         }
 
+        environment {
+            PROJECT_NAME = "${env.GIT_URL.tokenize('/').last().replace('.git', '')}"
+            BRANCH_NAME = 'feature' // Cambia si usas otro nombre de rama
+            SOURCE_PATH = './src' // Define el directorio donde se encuentran los archivos fuente
+            SCANNER_HOME=tool 'sonarscanner'
+        }
+
         stages {
             stage('Clone Repository') {
                 steps {
