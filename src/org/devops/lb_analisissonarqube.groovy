79f89c1bc14eb4153e5e0bf0dde605aea59d5f6b
MorosDaniel <dani28022016@gmail.com>
2025-01-14 15:49:06 -0500

    Clases serializable

--- src/org/devops/lb_analisissonarqube.groovy
+++ src/org/devops/lb_analisissonarqube.groovy
@@ -1,9 +1,27 @@
 package org.devops
 
-def clone(){
-    git branch: "${env.nameBranch}", url: "${env.UrlGitHub}"
-}
 
-def install(){
-    sh 'npm install'
+class GitOperations implements Serializable {
+    private def script
+
+    GitOperations(script) {
+        this.script = script
+    }
+
+    def clone() {
+        try {
+            script.git branch: "${script.env.nameBranch}", 
+                      url: "${script.env.UrlGitHub}"
+        } catch (Exception e) {
+            script.error "Error al clonar repositorio: ${e.getMessage()}"
+        }
+    }
+
+    def install() {
+        try {
+            script.sh 'npm install'
+        } catch (Exception e) {
+            script.error "Error durante npm install: ${e.getMessage()}"
+        }
+    }
 }
