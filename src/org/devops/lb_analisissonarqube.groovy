5aa0ce07a4712673d9543d69ef641168d474d425
MorosDaniel <dani28022016@gmail.com>
2025-01-14 16:17:10 -0500

    Ajuste de variables de entorno

--- src/org/devops/lb_analisissonarqube.groovy
+++ src/org/devops/lb_analisissonarqube.groovy
@@ -1,7 +1,7 @@
 package org.devops
 
 def cloneRepository(){
-    git branch: "${env.nameBranch}", url: "${env.UrlGitHub}"
+    git branch: "${env.GIT_BRANCH_1}", url: "${env.GIT_URL_1}"
 }
 
 def installNpm(){
