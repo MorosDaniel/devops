87dca6527fa4251aea523db341eafbcf170cbbe0
MorosDaniel <dani28022016@gmail.com>
2025-01-14 17:04:23 -0500

    Nueva manera

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,53 +1,52 @@
-import org.devops.lb_analisissonarqube
-import org.devops.lb_buildartefacto
+@library('devops')_
+import src.org.devops.lb_analisissonarqube
+import src.org.devops.lb_buildartefacto
 
-def call(Map config){
-    pipeline {
-        agent any
+pipeline {
+    agent any
 
-        tools {
-            nodejs "NodeJS" // NodeJS configurado en Jenkins
-            jdk "jdk"
-            maven  "maven3"
+    tools {
+        nodejs "NodeJS" // NodeJS configurado en Jenkins
+        jdk "jdk"
+        maven  "maven3"
+    }
+
+    environment {
+        PROJECT_NAME = "${env.GIT_URL.tokenize('/').last().replace('.git', '')}"
+        BRANCH_NAME = 'feature' // Cambia si usas otro nombre de rama
+        SOURCE_PATH = './src' // Define el directorio donde se encuentran los archivos fuente
+        SCANNER_HOME=tool 'sonarscanner'
+    }
+
+    stages {
+        stage('Clone Repository') {
+            steps {
+                lb_buildartefacto.cloneRepository()
+            }
         }
 
-        environment {
-            PROJECT_NAME = "${env.GIT_URL.tokenize('/').last().replace('.git', '')}"
-            BRANCH_NAME = 'feature' // Cambia si usas otro nombre de rama
-            SOURCE_PATH = './src' // Define el directorio donde se encuentran los archivos fuente
-            SCANNER_HOME=tool 'sonarscanner'
+        stage('Install Dependencies') {
+            steps {
+                script {
+                    BuildArt = new lb_buildartefacto();
+                    BuildArt.installNpm()
+                }
+            }
         }
 
-        stages {
-            stage('Clone Repository') {
-                steps {
-                    git branch: "main", url: "https://github.com/MorosDaniel/Petclinic"
+        stage('Run Tests and Check Coverage') {
+            steps {
+                script {
+                    AnalisisSonar = new lb_analisissonarqube();
+                    AnalisisSonar.testCoverage()
                 }
             }
+        }
 
-            stage('Install Dependencies') {
-                steps {
-                    script {
-                        BuildArt = new lb_buildartefacto();
-                        BuildArt.installNpm()
-                    }
-                }
-            }
-
-            stage('Run Tests and Check Coverage') {
-                steps {
-                    script {
-                        AnalisisSonar = new lb_analisissonarqube();
-                        AnalisisSonar.testCoverage()
-                    }
-                }
-            }
-
-            stage('SonarQube Analysis') {
-                steps {
-                    script {
-                        AnalisisSonar.analisisSonar()
-                    }
+        stage('SonarQube Analysis') {
+            steps {
+                script {
+                    AnalisisSonar.analisisSonar()
                 }
             }
         }
