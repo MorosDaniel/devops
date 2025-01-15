ba0515a8dab841bcee2f124369d5d5c3be585f6e
MorosDaniel <dani28022016@gmail.com>
2025-01-15 00:03:33 -0500

    Ya es mucho we

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -2,53 +2,55 @@
 def Sonar = library('devops@feature').org.devops.lb_buildartefacto
 def Build = library('devops@feature').org.devops.lb_buildartefacto
 
-pipeline {
-    
-    agent any
+def call{
+    pipeline {
+        
+        agent any
 
-    tools {
-        nodejs "NodeJS" // NodeJS configurado en Jenkins
-        jdk "jdk"
-        maven  "maven3"
-    }
-
-    environment {
-        PROJECT_NAME = "${env.GIT_URL.tokenize('/').last().replace('.git', '')}"
-        BRANCH_NAME = 'feature' // Cambia si usas otro nombre de rama
-        SOURCE_PATH = './src' // Define el directorio donde se encuentran los archivos fuente
-        SCANNER_HOME=tool 'sonarscanner'
-    }
-
-    stages {
-        stage('Clone Repository') {
-            steps {
-                script{
-                    
-                    Build.cloneRepository()
-                }
-            }
+        tools {
+            nodejs "NodeJS" // NodeJS configurado en Jenkins
+            jdk "jdk"
+            maven  "maven3"
         }
 
-        stage('Install Dependencies') {
-            steps {
-                script {
-                    Build.installNpm()
-                }
-            }
+        environment {
+            PROJECT_NAME = "${env.GIT_URL.tokenize('/').last().replace('.git', '')}"
+            BRANCH_NAME = 'feature' // Cambia si usas otro nombre de rama
+            SOURCE_PATH = './src' // Define el directorio donde se encuentran los archivos fuente
+            SCANNER_HOME=tool 'sonarscanner'
         }
 
-        stage('Run Tests and Check Coverage') {
-            steps {
-                script {
-                    Sonar.testCoverage()
+        stages {
+            stage('Clone Repository') {
+                steps {
+                    script{
+                        
+                        Build.cloneRepository()
+                    }
                 }
             }
-        }
 
-        stage('SonarQube Analysis') {
-            steps {
-                script {
-                    Sonar.analisisSonar()
+            stage('Install Dependencies') {
+                steps {
+                    script {
+                        Build.installNpm()
+                    }
+                }
+            }
+
+            stage('Run Tests and Check Coverage') {
+                steps {
+                    script {
+                        Sonar.testCoverage()
+                    }
+                }
+            }
+
+            stage('SonarQube Analysis') {
+                steps {
+                    script {
+                        Sonar.analisisSonar()
+                    }
                 }
             }
         }
