5ff0527cada519c2a2f5418be3ccfb0d61cf24b5
MorosDaniel <dani28022016@gmail.com>
2025-01-15 13:57:01 -0500

    fasd

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -2,53 +2,17 @@
 
 import org.devops.lb_buildartefacto
 
-def call() {
-    pipeline {
-        
-        agent any
+def build = new lb_buildartefacto()
 
-        tools {
-            nodejs "NodeJS"
-            jdk "jdk"
-            maven  "maven3"
-        }
-
-        environment {
-            BRANCH_NAME = 'feature' 
-            SCANNER_HOME=tool 'sonarscanner'
-        }
-
-        stages {
-            stage('Clone Repository') {
-                steps {
-                    script{
-                        lb_buildartefacto.cloneRepository(env.GIT_BRANCH_1,env.GIT_URL_1)
-                    }
-                }
+pipeline {
+    agent any
+    stages {
+        stage ('Construccion') {
+            when {
+                expression { build.cloneRepository() }
             }
-
-            stage('Install Dependencies') {
-                steps {
-                    script {
-                        Build.installNpm()
-                    }
-                }
-            }
-
-            stage('Run Tests and Check Coverage') {
-                steps {
-                    script {
-                        Sonar.testCoverage()
-                    }
-                }
-            }
-
-            stage('SonarQube Analysis') {
-                steps {
-                    script {
-                        Sonar.analisisSonar()
-                    }
-                }
+            steps {
+                echo "Hecho manito"
             }
         }
     }
