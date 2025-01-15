a439ccfdc1dc82acd2718cc169b7eafebfccabfa
MorosDaniel <dani28022016@gmail.com>
2025-01-15 13:58:15 -0500

    dsadsad

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -4,15 +4,17 @@ import org.devops.lb_buildartefacto
 
 def build = new lb_buildartefacto()
 
-pipeline {
-    agent any
-    stages {
-        stage ('Construccion') {
-            when {
-                expression { build.cloneRepository() }
-            }
-            steps {
-                echo "Hecho manito"
+def call() { 
+    pipeline {
+        agent any
+        stages {
+            stage ('Construccion') {
+                when {
+                    expression { build.cloneRepository() }
+                }
+                steps {
+                    echo "Hecho manito"
+                }
             }
         }
     }
