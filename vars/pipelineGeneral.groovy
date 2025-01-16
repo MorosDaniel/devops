9813e5648222f6c8de98d66b4aa401aea4795b68
MorosDaniel <dani28022016@gmail.com>
2025-01-16 15:33:17 -0500

    njgrhgsgsl

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -13,30 +13,30 @@ def call() {
         }
 
         stages {
-            // stage ('Construccion') {
-            //     steps{
-            //         script {
-            //             def build = new org.devops.lb_buildartefacto()
-            //             build.cloneRepository()
-            //         }
-            //     }
-            // }
-            // stage ('Instalar dependencias') {
-            //     steps{
-            //         script {
-            //             def build = new org.devops.lb_buildartefacto()
-            //             build.installNpm()
-            //         }
-            //     }
-            // }
-            // stage ('test') {
-            //     steps{
-            //         script {
-            //             def sonar = new org.devops.lb_analisissonarqube()
-            //             sonar.testCoverage()
-            //         }
-            //     }
-            // }
+            stage ('Construccion') {
+                steps{
+                    script {
+                        def build = new org.devops.lb_buildartefacto()
+                        build.cloneRepository()
+                    }
+                }
+            }
+            stage ('Instalar dependencias') {
+                steps{
+                    script {
+                        def build = new org.devops.lb_buildartefacto()
+                        build.installNpm()
+                    }
+                }
+            }
+            stage ('test') {
+                steps{
+                    script {
+                        def sonar = new org.devops.lb_analisissonarqube()
+                        sonar.testCoverage()
+                    }
+                }
+            }
             stage ('analisis en sonar') {
                 steps{
                     script {
