c66bef1bd8fb87b687662cedda723012ae654105
MorosDaniel <dani28022016@gmail.com>
2025-01-16 16:10:41 -0500

    hola cara de bola

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -12,31 +12,31 @@ def call() {
             SONARNAME = 'react-test-jenkinsfile'
         }
 
-        stages {
-            stage ('Construccion') {
-                steps{
-                    script {
-                        def build = new org.devops.lb_buildartefacto()
-                        build.cloneRepository()
-                    }
-                }
-            }
-            stage ('Instalar dependencias') {
-                steps{
-                    script {
-                        def build = new org.devops.lb_buildartefacto()
-                        build.installNpm()
-                    }
-                }
-            }
-            stage ('test') {
-                steps{
-                    script {
-                        def sonar = new org.devops.lb_analisissonarqube()
-                        sonar.testCoverage()
-                    }
-                }
-            }
+        // stages {
+        //     stage ('Construccion') {
+        //         steps{
+        //             script {
+        //                 def build = new org.devops.lb_buildartefacto()
+        //                 build.cloneRepository()
+        //             }
+        //         }
+        //     }
+        //     stage ('Instalar dependencias') {
+        //         steps{
+        //             script {
+        //                 def build = new org.devops.lb_buildartefacto()
+        //                 build.installNpm()
+        //             }
+        //         }
+        //     }
+        //     stage ('test') {
+        //         steps{
+        //             script {
+        //                 def sonar = new org.devops.lb_analisissonarqube()
+        //                 sonar.testCoverage()
+        //             }
+        //         }
+        //     }
             stage ('analisis en sonar') {
                 steps{
                     script {
