1b6681f604714c6090d21883c1b5706521dbcc49
MorosDaniel <dani28022016@gmail.com>
2025-01-19 20:22:44 -0500

    dasdad

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -1,7 +1,7 @@
 package org.devops
 
 def analisisOwasp(proyectGitName){
-    sh """ docker run -rm -v ProyectOwasp:/zap/wrk/:rw \
+    sh """ docker run --rm -v ProyectOwasp:/zap/wrk/:rw \
          --user root --network=${env.NameNetwork} \
          -t owasp/zap2docker-stable \
          zap-full-scan.py \
