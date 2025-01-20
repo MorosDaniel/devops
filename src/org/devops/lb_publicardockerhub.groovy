0db53774082797bd05740aea6bbd82b3a7f8bc6b
MorosDaniel <dani28022016@gmail.com>
2025-01-19 20:11:48 -0500

    RGEARGE

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -4,7 +4,7 @@ def publicarImage(proyectGitName){
     withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}",
     passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
         sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
-        sh "docker tag ${proyectGitName} ${env.DOCKERHUB_USERNAME}/${proyectGitName}"
+        // sh "docker tag ${proyectGitName} ${env.DOCKERHUB_USERNAME}/${proyectGitName}"
         sh "docker push ${env.DOCKERHUB_USERNAME}/${proyectGitName}"
     }
 }
