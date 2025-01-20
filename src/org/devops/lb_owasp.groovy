ba012edca60f333164b340e9ca80ef1a0e530d98
MorosDaniel <dani28022016@gmail.com>
2025-01-19 20:26:03 -0500

    ferafea

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -3,7 +3,7 @@ package org.devops
 def analisisOwasp(proyectGitName){
     sh """ docker run --rm -v ProyectOwasp:/zap/wrk/:rw \
          --user root --network=${env.NameNetwork} \
-         -t owasp/zap2docker-stable \
+         -t zaproxy/zap-weekly \
          zap-full-scan.py \
          -t ${env.dominio} \
          -r ProyectOwasp.html -I
